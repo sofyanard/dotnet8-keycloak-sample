238dca527e28fac7be73051b069cdefa5dbee40f
Sofyan Ardianto <sofyanard@gmail.com>
2024-08-16 10:32:07 +0700

    Add project files.

---
+++ dotnet8-keycloak-sample/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using dotnet8_keycloak_sample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet8_keycloak_sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ dotnet8-keycloak-sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet8_keycloak_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
