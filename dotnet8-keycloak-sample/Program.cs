1cdb6de2718bed5db48b8e6f9274d801a650b922
Sofyan Ardianto <sofyanard@gmail.com>
2025-06-18 16:13:38 +0700

    redirect after authentication successful (12) :goat:

--- dotnet8-keycloak-sample/Program.cs
+++ dotnet8-keycloak-sample/Program.cs
@@ -81,9 +81,10 @@ namespace dotnet8_keycloak_sample
                     // Handle the callback from Keycloak after successful authentication
                     // await context.Response.WriteAsync("Authentication successful");
                     logger.LogInformation("Authentication successful");
-                    context.Response.Redirect("/home/index");
-                    logger.LogInformation("Redirecting to /home/index");
+                    context.Response.Redirect("/home/privacy");
+                    logger.LogInformation("Redirecting to /home/privacy");
                     await Task.CompletedTask;
+                    logger.LogInformation("CompletedTask");
                 });
             });
 
